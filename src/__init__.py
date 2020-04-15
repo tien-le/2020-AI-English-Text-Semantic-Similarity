1a97afbb5b96d14bf2137e38755a728049f6caec
wjunnneg <1194348056@qq.com>
2020-04-15 15:46:35 +0800

    update 拼写检查.

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+import re
+
+
+
+
+
+print(replace_typical_misspell("I'd say it primarily depends on two things:"))
--- src/libs/utils.py
+++ src/libs/utils.py
@@ -6,6 +6,7 @@ from rouge import Rouge
 
 sys.path.append(os.path.abspath('.'))
 os.chdir(sys.path[0])
+import re
 import copy
 import pandas as pd
 import numpy as np
@@ -16,6 +17,78 @@ from sklearn.model_selection import train_test_split
 class Utils(object):
     def __init__(self):
         pass
+
+    @staticmethod
+    def replace_typical_misspell(text):
+        mispell_dict = {'colour': 'color', 'centre': 'center', 'didnt': 'did not', 'doesnt': 'does not',
+                        'isnt': 'is not', 'shouldnt': 'should not', 'favourite': 'favorite', 'travelling': 'traveling',
+                        'counselling': 'counseling', 'theatre': 'theater', 'cancelled': 'canceled', 'labour': 'labor',
+                        'organisation': 'organization', 'wwii': 'world war 2', 'citicise': 'criticize',
+                        'instagram': 'social medium',
+                        'whatsapp': 'social medium', 'snapchat': 'social medium', "ain't": "is not",
+                        "aren't": "are not", "can't": "cannot", "'cause": "because", "could've": "could have",
+                        "couldn't": "could not", "didn't": "did not", "doesn't": "does not", "don't": "do not",
+                        "hadn't": "had not", "hasn't": "has not", "haven't": "have not", "he'd": "he would",
+                        "he'll": "he will", "he's": "he is", "how'd": "how did", "how'd'y": "how do you",
+                        "how'll": "how will", "how's": "how is", "I'd": "I would", "I'd've": "I would have",
+                        "I'll": "I will", "I'll've": "I will have", "I'm": "I am", "I've": "I have",
+                        "i'd": "i would", "i'd've": "i would have", "i'll": "i will", "i'll've": "i will have",
+                        "i'm": "i am", "i've": "i have", "isn't": "is not", "it'd": "it would",
+                        "it'd've": "it would have", "it'll": "it will", "it'll've": "it will have",
+                        "it's": "it is", "let's": "let us", "ma'am": "madam", "mayn't": "may not",
+                        "might've": "might have", "mightn't": "might not", "mightn't've": "might not have",
+                        "must've": "must have", "mustn't": "must not", "mustn't've": "must not have",
+                        "needn't": "need not", "needn't've": "need not have", "o'clock": "of the clock",
+                        "oughtn't": "ought not", "oughtn't've": "ought not have", "shan't": "shall not",
+                        "sha'n't": "shall not", "shan't've": "shall not have", "she'd": "she would",
+                        "she'd've": "she would have", "she'll": "she will", "she'll've": "she will have",
+                        "she's": "she is", "should've": "should have", "shouldn't": "should not",
+                        "shouldn't've": "should not have",
+                        "so've": "so have", "so's": "so as", "this's": "this is", "that'd": "that would",
+                        "that'd've": "that would have", "that's": "that is",
+                        "there'd": "there would", "there'd've": "there would have", "there's": "there is",
+                        "here's": "here is", "they'd": "they would", "they'd've": "they would have",
+                        "they'll": "they will", "they'll've": "they will have",
+                        "they're": "they are", "they've": "they have", "to've": "to have",
+                        "wasn't": "was not", "we'd": "we would", "we'd've": "we would have",
+                        "we'll": "we will", "we'll've": "we will have", "we're": "we are",
+                        "we've": "we have", "weren't": "were not", "what'll": "what will",
+                        "what'll've": "what will have", "what're": "what are", "what's": "what is",
+                        "what've": "what have", "when's": "when is", "when've": "when have",
+                        "where'd": "where did", "where's": "where is", "where've": "where have",
+                        "who'll": "who will", "who'll've": "who will have", "who's": "who is",
+                        "who've": "who have", "why's": "why is", "why've": "why have",
+                        "will've": "will have", "won't": "will not", "won't've": "will not have",
+                        "would've": "would have", "wouldn't": "would not", "wouldn't've": "would not have",
+                        "y'all": "you all", "y'all'd": "you all would", "y'all'd've": "you all would have",
+                        "y'all're": "you all are", "y'all've": "you all have", "you'd": "you would",
+                        "you'd've": "you would have", "you'll": "you will", "you'll've": "you will have",
+                        "you're": "you are", "you've": "you have", 'colour': 'color', 'centre': 'center',
+                        'favourite': 'favorite', 'travelling': 'traveling', 'counselling': 'counseling',
+                        'theatre': 'theater', 'cancelled': 'canceled', 'labour': 'labor',
+                        'organisation': 'organization', 'wwii': 'world war 2', 'citicise': 'criticize',
+                        'youtu ': 'youtube ', 'Qoura': 'Quora', 'sallary': 'salary', 'Whta': 'What',
+                        'narcisist': 'narcissist', 'howdo': 'how do', 'whatare': 'what are',
+                        'howcan': 'how can', 'howmuch': 'how much', 'howmany': 'how many', 'whydo': 'why do',
+                        'doI': 'do I', 'theBest': 'the best', 'howdoes': 'how does',
+                        'mastrubation': 'masturbation', 'mastrubate': 'masturbate',
+                        "mastrubating": 'masturbating', 'pennis': 'penis', 'Etherium': 'Ethereum',
+                        'narcissit': 'narcissist', 'bigdata': 'big data', '2k17': '2017', '2k18': '2018',
+                        'qouta': 'quota', 'exboyfriend': 'ex boyfriend', 'airhostess': 'air hostess',
+                        "whst": 'what', 'watsapp': 'whatsapp', 'demonitisation': 'demonetization',
+                        'demonitization': 'demonetization', 'demonetisation': 'demonetization'
+                        }
+
+        def _replace(match):
+            return mispellings[match.group(0)]
+
+        def _get_mispell(mispell_dict):
+            mispell_re = re.compile('(%s)' % '|'.join(mispell_dict.keys()))
+            return mispell_dict, mispell_re
+
+        mispellings, mispellings_re = _get_mispell(mispell_dict)
+
+        return mispellings_re.sub(_replace, text.lower())
 
     def match(self, data):
         result = pd.DataFrame(columns=['label'], data=data['label'])
@@ -75,10 +148,10 @@ class Utils(object):
             train_tsv_data['label'] = train_data[:, 2]
 
             # 改成小写
-            dev_tsv_data['text_a'] = dev_tsv_data['text_a'].apply(lambda a: a.lower())
-            dev_tsv_data['text_b'] = dev_tsv_data['text_b'].apply(lambda a: a.lower())
-            train_tsv_data['text_a'] = train_tsv_data['text_a'].apply(lambda a: a.lower())
-            train_tsv_data['text_b'] = train_tsv_data['text_b'].apply(lambda a: a.lower())
+            dev_tsv_data['text_a'] = dev_tsv_data['text_a'].apply(lambda a: Utils.replace_typical_misspell(a))
+            dev_tsv_data['text_b'] = dev_tsv_data['text_b'].apply(lambda a: Utils.replace_typical_misspell(a))
+            train_tsv_data['text_a'] = train_tsv_data['text_a'].apply(lambda a: Utils.replace_typical_misspell(a))
+            train_tsv_data['text_b'] = train_tsv_data['text_b'].apply(lambda a: Utils.replace_typical_misspell(a))
 
             dev_tsv_data = pd.concat(
                 [dev_tsv_data[['text_a', 'text_b', 'label']], dev_tsv_data[['text_b', 'text_a', 'label']]],
@@ -101,8 +174,8 @@ class Utils(object):
                                                                index=None, sep='\t',
                                                                encoding='utf-8')
             train_tsv_data[['text_a', 'text_b', 'label']].to_csv(os.path.join(fold_index_dir, 'train.tsv'), header=None,
-                                                               index=None, sep='\t',
-                                                               encoding='utf-8')
+                                                                 index=None, sep='\t',
+                                                                 encoding='utf-8')
 
     def generate_train_dev_test(self, train_csv_path, test_csv_path, max_sequence_length):
         """
@@ -221,7 +294,7 @@ if __name__ == '__main__':
     #     max_sequence_length=128
     # )
 
-    util.generate_fold_train_dev(train_csv_path='../../data/input/train.csv', test_csv_path='../../data/input/test.csv',
-                                 fold_dir='../../data/fold')
+    # util.generate_fold_train_dev(train_csv_path='../../data/input/train.csv', test_csv_path='../../data/input/test.csv',
+    #                              fold_dir='../../data/fold')
 
     # util.generate_keys_csv()
