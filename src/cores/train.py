84538ffe38233b52d9ba873284bfbed35b13d463
wjunnneg <1194348056@qq.com>
2020-04-16 19:52:15 +0800

    update run.py.

--- src/cores/train.py
+++ src/cores/train.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from __future__ import absolute_import, division, print_function
 import os
 import sys
@@ -5,13 +6,38 @@ import sys
 sys.path.append(os.path.abspath('.'))
 os.chdir(sys.path[0])
 
-from src.cores.run import *
+import argparse
+import glob
+import json
+import logging
+import os
+import random
+from dataclasses import dataclass, field
+from typing import Optional
+
+import shutil
+import numpy as np
+import torch
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler, TensorDataset
+from torch.utils.data.distributed import DistributedSampler
+from tqdm import tqdm, trange
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
 from src.transformers import (MODEL_FOR_SEQUENCE_CLASSIFICATION_MAPPING,
+                              WEIGHTS_NAME,
                               AdamW,
+                              AutoConfig,
+                              AutoModelForSequenceClassification,
+                              AutoTokenizer,
+                              HfArgumentParser,
+                              TrainingArguments,
                               get_linear_schedule_with_warmup,
                               )
-
-from src.cores import evaluate
+from src.transformers import glue_compute_metrics as compute_metrics
+from src.transformers import glue_convert_examples_to_features as convert_examples_to_features
+from src.transformers import glue_output_modes as output_modes
+from src.transformers import glue_processors as processors
 
 try:
     from torch.utils.tensorboard import SummaryWriter
@@ -24,6 +50,81 @@ MODEL_CONFIG_CLASSES = list(MODEL_FOR_SEQUENCE_CLASSIFICATION_MAPPING.keys())
 MODEL_TYPES = tuple(conf.model_type for conf in MODEL_CONFIG_CLASSES)
 
 ALL_MODELS = sum((tuple(conf.pretrained_config_archive_map.keys()) for conf in MODEL_CONFIG_CLASSES), (), )
+
+
+def set_seed(args):
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    if args.n_gpu > 0:
+        torch.cuda.manual_seed_all(args.seed)
+
+
+def load_and_cache_examples(args, task, tokenizer, evaluate=False, predict=False):
+    if args.local_rank not in [-1, 0] and not evaluate:
+        # Make sure only the first process in distributed training process the dataset,
+        # and the others will use the cache
+        torch.distributed.barrier()
+
+    processor = processors[task]()
+    output_mode = output_modes[task]
+
+    key = 'train'
+    if evaluate:
+        key = 'dev'
+    if predict:
+        key = 'test'
+
+    # Load data features from cache or dataset file
+    cached_features_file = os.path.join(
+        args.data_dir,
+        "cached_{}_{}_{}_{}".format(key,
+                                    list(filter(None, args.model_name_or_path.split("/"))).pop(),
+                                    str(args.max_seq_length),
+                                    str(task),
+                                    ),
+    )
+    if os.path.exists(cached_features_file) and not args.overwrite_cache:
+        logger.info("Loading features from cached file %s", cached_features_file)
+        features = torch.load(cached_features_file)
+    else:
+        logger.info("Creating features from dataset file at %s", args.data_dir)
+        label_list = processor.get_labels()
+        if task in ["mnli", "mnli-mm"] and args.model_type in ["roberta", "xlmroberta"]:
+            # HACK(label indices are swapped in RoBERTa pretrained model)
+            label_list[1], label_list[2] = label_list[2], label_list[1]
+
+        if evaluate:
+            examples = processor.get_dev_examples(args.data_dir)
+        elif predict:
+            examples = processor.get_test_examples(args.data_dir)
+        else:
+            examples = processor.get_train_examples(args.data_dir)
+
+        features = convert_examples_to_features(
+            examples, tokenizer, max_length=args.max_seq_length, label_list=label_list, output_mode=output_mode,
+        )
+        # if args.local_rank in [-1, 0]:
+        #     logger.info("Saving features into cached file %s", cached_features_file)
+        #     torch.save(features, cached_features_file)
+
+    if args.local_rank == 0 and not evaluate:
+        # Make sure only the first process in distributed training process the dataset,
+        # and the others will use the cache
+        torch.distributed.barrier()
+
+    # Convert to Tensors and build dataset
+    all_input_ids = torch.tensor([f.input_ids for f in features], dtype=torch.long)
+    all_attention_mask = torch.tensor([f.attention_mask for f in features], dtype=torch.long)
+    all_token_type_ids = torch.tensor([f.token_type_ids for f in features], dtype=torch.long)
+    if output_mode == "classification":
+        all_labels = torch.tensor([f.label for f in features], dtype=torch.long)
+    elif output_mode == "regression":
+        all_labels = torch.tensor([f.label for f in features], dtype=torch.float)
+
+    dataset = TensorDataset(all_input_ids, all_attention_mask, all_token_type_ids, all_labels)
+
+    return dataset
 
 
 def train(args, train_dataset, model, tokenizer):
